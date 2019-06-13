9dc257c2f4d08101a3385469fe66c130b5e84839
dealiaxy <dealiaxy@gmail.com>
2019-06-13 21:58:04 +0800

    something in PostController

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -13,6 +13,32 @@ namespace DreamingHome.Controllers
     [ApiController]
     public class PostController : ControllerBase
     {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var model = new PostViewModel();
+            using (var context = new MainContext())
+            {
+                var sqlData = new SqlPostData(context);
+                model.Posts = sqlData.GetAll();
+            }
+
+            return new JsonResult(model.Posts);
+        }
+
+        [HttpPost("add")]
+        public ActionResult<Response> Add([FromBody] Post post)
+        {
+            if (post == null)
+                return new Response {Message = "没提供内容啊！", Success = false};
+            using (var context = new MainContext())
+            {
+                var sqlData = new SqlPostData(context);
+                var result = sqlData.Add(post);
+                var data = new {Post = post};
+                return new Response(true, result ? "发表成功！" : "发表失败！", data);
+            }
+        }
     }
 
     public class SqlPostData
@@ -38,6 +64,22 @@ namespace DreamingHome.Controllers
         public List<Post> GetAll()
         {
             return _context.Posts.ToList<Post>();
+        }
+    }
+
+    public class SqlPictureData
+    {
+        private MainContext _context { get; set; }
+
+        public SqlPictureData(MainContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(PostPicture picture)
+        {
+            _context.Add(picture);
+            return _context.SaveChanges() > 0;
         }
     }
 }
